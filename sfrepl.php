07ee20129435387c54d4ad44d69f323fb7f5e1b7
Mark Biek <support@janustech.net>
2018-05-01 16:16:20 -0400

    Initial repl loop

--- sfrepl.php
+++ sfrepl.php
@@ -14,3 +14,19 @@ $dotenv = new Dotenv();
 $dotenv->load(__DIR__ . '/.env');
 
 $api = new Salesforce();
+
+$bdone = false;
+
+$exits = ['q', 'exit', 'quit'];
+while (!$bdone) {
+    $line = readline('>');
+    if (!empty($line)) {
+        readline_add_history($line);
+    }
+
+    if (in_array($line, $exits)) {
+        $bdone = true;
+    } else {
+        //TODO
+    }
+}
